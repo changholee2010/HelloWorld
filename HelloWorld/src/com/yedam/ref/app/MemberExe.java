f9e12b9f2a1011bdccbb9c9230ef94c2279db067
admin <pc@pc.com>
2025-05-19 16:45:35 +0900

    회원관련 연습.

---
+++ HelloWorld/src/com/yedam/ref/app/MemberExe.java
@@ -0,0 +1,20 @@
+package com.yedam.ref.app;
+
+/*
+ * 회원 추가,수정,삭제,조회 기능.
+ * 1)추가
+ *   - 아이디,이름,전화번호,포인트
+ * 2)수정
+ *   - 아이디 
+ *   - 전화번호 (바뀔항목)
+ * 3)삭제
+ *   - 아이디
+ * 4)조회
+ *   - 이름 (조회)
+ */
+
+public class MemberExe {
+	public static void main(String[] args) {
+		
+	}
+}
