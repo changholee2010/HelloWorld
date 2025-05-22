e932303b4558f11fdad80172f7712b69c41dc433
admin <pc@pc.com>
2025-05-22 15:33:37 +0900

    done

--- HelloClass/src/com/yedam/Calendar.java
+++ HelloClass/src/com/yedam/Calendar.java
@@ -1,7 +1,22 @@
 package com.yedam;
 
 public class Calendar {
-	
+
+	static boolean isLeapYear(int year) {
+		// 윤년이면 true, 평년이면 false.
+		boolean leapYear = true;
+		if (year % 4 != 0) {
+			leapYear = false;
+		}
+		if (leapYear && year % 100 == 0) {
+			leapYear = false;
+		}
+		if (!leapYear && year % 400 == 0) {
+			leapYear = true;
+		}
+		return leapYear;
+	} // end of isLeapYear.
+
 	// 1년 365일에 해당하는 월별 날짜를 배열에 저장하고
 	// 출력하고자 하는 월의 내용만 화면에 출력한다.
 	public static void showMonth(int month) {
@@ -12,7 +27,7 @@ public class Calendar {
 		int idx = 0; // 날짜를 지정할때 사용할 인덱스를 증가.
 		int spaces = 3; // 1월달의 공백갯수. 월마다 계산하기.
 
-		//  열두달의 각 날짜를 배열에 저장.
+		// 열두달의 각 날짜를 배열에 저장.
 		for (int i = 0; i < lastDateAry.length; i++) {
 			int lastDate = lastDateAry[i]; // 월별 말일을 가지고 온다.
 			for (int j = 1; j <= lastDate; j++) { // 1일부터 말일을 저장.
